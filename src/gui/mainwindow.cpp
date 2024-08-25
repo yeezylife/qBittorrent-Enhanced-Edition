ecfd6c48dbb1611cc2d7e00c499bef67c4756cda
yeezylife <30175425+yeezylife@users.noreply.github.com>
2024-08-25 08:16:22 +0800

    Update mainwindow.cpp

--- src/gui/mainwindow.cpp
+++ src/gui/mainwindow.cpp
@@ -137,7 +137,7 @@ MainWindow::MainWindow(IGUIApplication *app, WindowState initialState)
 
     Preferences *const pref = Preferences::instance();
     m_uiLocked = pref->isUILocked();
-    setWindowTitle(QStringLiteral("qBittorrent Enhanced Edition " QBT_VERSION));
+    setWindowTitle(QStringLiteral("qBittorrent " QBT_VERSION));
     m_displaySpeedInTitle = pref->speedInTitleBar();
     // Setting icons
 #ifndef Q_OS_MACOS
@@ -1555,7 +1555,7 @@ void MainWindow::reloadSessionStats()
 
     if (m_displaySpeedInTitle)
     {
-        setWindowTitle(tr("[D: %1, U: %2] qBittorrent Enhanced Edition %3", "D = Download; U = Upload; %3 is qBittorrent version")
+        setWindowTitle(tr("[D: %1, U: %2] qBittorrent %3", "D = Download; U = Upload; %3 is qBittorrent version")
             .arg(Utils::Misc::friendlyUnit(status.payloadDownloadRate, true)
                 , Utils::Misc::friendlyUnit(status.payloadUploadRate, true)
                 , QStringLiteral(QBT_VERSION)));
@@ -1684,7 +1684,7 @@ void MainWindow::on_actionSpeedInTitleBar_triggered()
     if (m_displaySpeedInTitle)
         reloadSessionStats();
     else
-        setWindowTitle(QStringLiteral("qBittorrent Enhanced Edition " QBT_VERSION));
+        setWindowTitle(QStringLiteral("qBittorrent " QBT_VERSION));
 }
 
 void MainWindow::on_actionRSSReader_triggered()
